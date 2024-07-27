5195f0f0a0557b2b6897876b55391b7e81b5bf5c
vdesimone <173499012+vdesimone@users.noreply.github.com>
2024-07-26 21:58:57 -0400

    edited the task html and addTask()

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function addTask() {
   const task = `
-    <div id="task-1" class="task">
+    <div class="task">
       <div class="check-btn-container">
-        <button class="check-btn"><i class="fa-solid fa-check" style="display: none;"></i></button>
+        <button onclick="checkTask()" class="check-btn" id="check-btn"><i class="fa-solid fa-check" id="check-icon" style="display: none;"></i></button>
       </div>
       <div class="task-description">
-          <h3>Task 1</h3>
-          <p>description</p>
+        <h3>Task</h3>
+        <p>description</p>
       </div>
       <div class="task-btns-container">
         <button class="edit-btn"><i class="fa-solid fa-pencil"></i></button>
